72f8c1dce298eccf66f66e99e8fbe6c1cf550348
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-01 19:45:35 +0200

    Fibonacci
    Added Fibonacci with closing and recursion

---
+++ Fibonacci_closing_recursion.py
@@ -0,0 +1,22 @@
+def caching_fibonacci():
+
+    cache = {}
+    
+    def fibonacci(n):
+
+        if n == 0:
+            cache.update({0: 0})
+        elif n == 1:
+            cache.update({1: 1})
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci (n - 2)
+        return cache[n]
+    
+    return fibonacci 
+
+fib = caching_fibonacci()
+num = fib(8)
+print(num)
+
