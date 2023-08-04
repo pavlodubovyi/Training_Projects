e21fafe422aa5c6fde587e08888454a533a2ecb6
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-04 13:43:46 +0200

    practice

--- practice.py
+++ practice.py
@@ -1,12 +1,40 @@
-from functools import reduce
+# import argparse
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("hello", help="Bot will offer its help")
+# args = parser.parse_args()
+
+# if __name__ == "__main__":
+#     pass
+
+phone_dict = {}
+
+name_number_str = input("Enter name and number (Name, space, number): ")
+name_number_list = name_number_str.split(" ")
+phone_dict.update({name_number_list[0]: name_number_list[1]})
+print(phone_dict)
 
 
-def amount_payment(payment):
+def format_phone_number(func):
+    def wrapper(*args, **kwargs):
+        if len(func(*args, **kwargs)) == 9:  # написав для польских номерів
+            result = "+48" + func(*args, **kwargs)
+        elif len(func(*args, **kwargs)) == 11:
+            result = "+" + func(*args, **kwargs)
+            return result
 
-    positive_payment_list = filter(lambda x: x > 0, payment)
-    return reduce(lambda x, y: x + y, positive_payment_list)
-    
+    return wrapper
 
-payment = [1, -3, 4, 2, 3, -7, 5]
 
-print(amount_payment(payment))
+@format_phone_number
+def sanitize_phone_number(phone):
+    new_phone = (
+        phone.strip()
+        .removeprefix("+")
+        .replace("(", "")
+        .replace(")", "")
+        .replace("-", "")
+        .replace(" ", "")
+    )
+    return new_phone
