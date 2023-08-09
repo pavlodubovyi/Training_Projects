c03b385721a791fc11495ae6ae9d7f1c4ff5e06b
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-09 09:44:00 +0200

    Update module_10.py

--- module_10.py
+++ module_10.py
@@ -8,31 +8,30 @@ class Contacts:
         return self.contacts
 
     def add_contacts(self, name, phone, email, favorite):
-        self.contacts.append(
-            {
-                "id": Contacts.current_id,
-                "name": name,
-                "phone": phone,
-                "email": email,
-                "favorite": favorite,
+
+        contact =  {
+            "id": Contacts.current_id,
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "favorite": favorite,
             }
-        )
+        
+        self.contacts.append(contact)
         Contacts.current_id += 1
-
-    def get_contact_by_id(self, id):
-        result = list(filter(lambda contact: contact.get("id") == id, self.contacts))
-        return result[0] if len(result) > 0 else None
-
-    def remove_contacts(self, id):
-        for el in self.contacts:
-            if el["id"] == id:
-                self.contacts.remove(el)
-
-
+        
+        # return self.contacts
     
-first_person = Contacts()
-second_person = Contacts()
-first_person.add_contacts("Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)
-second_person.add_contacts('Cyrus Jackson', '(501) 472-5218', 'nibh@semsempererat.com', False)
-contacts = Contacts.list_contacts
-print(contacts)
+all_persons = Contacts() # зробимо один спільний обькт классу Contacts
+
+all_persons.add_contacts("Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True) # додали один контакт
+all_persons.add_contacts('Cyrus Jackson', '(501) 472-5218', 'nibh@semsempererat.com', False) # додали другий
+
+print(all_persons.list_contacts) # тут будуть вже 2 елемента
+
+
+
+
+
+
+
