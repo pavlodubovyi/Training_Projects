86b2b346a20bd1247132c46689fbc63a514a92db
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-14 20:13:22 +0200

    5643

--- codewars_more.py
+++ codewars_more.py
@@ -1,19 +1,10 @@
-class Student:
+class Test:
 
-    def __init__(self, first_name, last_name, grades=[]):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.grades = grades
-    
-    def add_grade(self, grade):
-        self.grades.append(grade)
-    
-    def get_average(self):
-        return sum(self.grades) / len(self.grades)
+    def __init__(self):
+        self.name = "datacamp"
+        self._num = 7
 
-matthewConnorGrades = [44, 53, 27, 60]
-chloeMadisonGrades = [79, 58, 30, 66]
-studentGrades = matthewConnorGrades, chloeMadisonGrades
-matthewConnor = Student('Matthew', 'Connor', matthewConnorGrades)
-chloeMadison = Student('Chloe', 'Madison', chloeMadisonGrades)
-students = matthewConnor, chloeMadison
+obj = Test()
+print(obj.name)
+print(obj._num)
+
---
+++ module_11.py
@@ -0,0 +1,4 @@
+class Point:
+    def __init__(self, x, y) -> None:
+        self.__x = x
+        self.__y = y
