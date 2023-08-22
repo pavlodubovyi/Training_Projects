1c17194d2071339d00bfc9648d12ec96b4643bf4
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-22 18:38:00 +0200

    end_flag
    with end_flag

--- codewars.py
+++ codewars.py
@@ -1,3 +1,13 @@
-a = [1, 2]
-b = [3, 4]
-print(a + b)
+def multiiter(*params):
+    if len(params) == 1:
+        n = *params
+        for i in range(int(params)):
+            yield tuple(i)
+    elif len(params) == 2:
+        for i in [params][0]:
+            for j in [params][1]:
+                yield (i, j)
+
+
+for item in multiiter(2, 3):
+    print(item)
--- codewars_more.py
+++ codewars_more.py
@@ -1,10 +1,10 @@
-class Test:
 
-    def __init__(self):
-        self.name = "datacamp"
-        self._num = 7
-
-obj = Test()
-print(obj.name)
-print(obj._num)
-
+def no_of_argu(*args):
+   
+    # using len() method in args to count
+    return(len(args))
+ 
+print(no_of_argu(2, 5, 4))
+print(no_of_argu(4, 5, 6, 5, 4, 4))
+print(no_of_argu(3, 2, 32, 4, 4, 52, 1))
+print(no_of_argu(1))
