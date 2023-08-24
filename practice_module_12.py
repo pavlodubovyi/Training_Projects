edb64cff5a7c445e5aa5cb968fe500d7b77fe8a4
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-24 20:18:52 +0200

    dfg

--- practice_module_12.py
+++ practice_module_12.py
@@ -1,6 +1,56 @@
 import csv
 
-with open("eggs.csv", "w", newline="") as fh:
-    spam_writer = csv.writer(fh)
-    spam_writer.writerow(["Spam"] * 5 + ["Baked Beans"])
-    spam_writer.writerow(["Spam", "Lovely Spam", "Wonderful Spam"])
+contacts = [
+    {
+        "name": "Allen Raymond",
+        "email": "nulla.ante@vestibul.co.uk",
+        "phone": "(992) 914-3792",
+        "favorite": False,
+    },
+    {
+        "name": "Pavlo Dubovyi",
+        "email": "pavlodubovyi@yahoo.com",
+        "phone": "1234567890",
+        "favorite": True,
+    },
+    {
+        "name": "John Doe",
+        "email": "john.doe@nowhere.to.go",
+        "phone": None,
+        "favorite": False,
+    },
+]
+
+filename = "my_contacts.csv"
+
+
+def write_contacts_to_file(filename, contacts):
+    field_names = []
+    for el in contacts:
+        for key in el.keys():
+            field_names.append(key)
+        break
+
+    with open(filename, "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(contacts)
+
+
+def read_contacts_from_file(filename):
+    contact_list = []
+    with open(filename, "r", newline="") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            contact = {
+                "name": row["name"],
+                "email": row["email"],
+                "phone": row["phone"],
+                "favorite": row["favorite"] == "True",
+            }
+            contact_list.append(contact)
+    return contact_list
+
+
+write_contacts_to_file(filename, contacts)
+print(read_contacts_from_file(filename))
