3a0ec954a14293d7175f0f6ffb231be6376a90d7
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-25 17:30:31 +0200

    wer

--- practice_module_12.py
+++ practice_module_12.py
@@ -1,17 +1,3 @@
-# import pickle
-
-
-# class Human:
-#     def __init__(self, name):
-#         self.name = name
-
-
-# bob = Human("Bob")
-# encoded_bob = pickle.dumps(bob)
-
-# decoded_bob = pickle.loads(encoded_bob)
-
-# bob.name == decoded_bob.name  # True
 import pickle
 
 
@@ -21,22 +7,46 @@ class Person:
         self.email = email
         self.phone = phone
         self.favorite = favorite
-        
-  
+
 
 class Contacts:
     def __init__(self, filename: str, contacts: list[Person] = None):
         if contacts is None:
             contacts = []
-        elif contacts:
-            
-        
+        self.filename = filename
+        self.contacts = contacts
 
     def save_to_file(self):
-        
-            
+        with open(self.filename, "wb") as file:
+            object = pickle.dump(self, file)
+        return object
 
     def read_from_file(self):
-        
-            
-        
+        with open(self.filename, "rb") as file:
+            return pickle.load(file)
+
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+
+
+persons = Contacts("user_class.dat", contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons == person_from_file)  # False
+print(persons.contacts[0] == person_from_file.contacts[0])  # False
+print(persons.contacts[0].name == person_from_file.contacts[0].name)  # True
+print(persons.contacts[0].email == person_from_file.contacts[0].email)  # True
+print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True
