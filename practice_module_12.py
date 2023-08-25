0a53866efa7dd512a133107b37aff4b73885bbee
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-25 16:21:52 +0200

    Update practice_module_12.py

--- practice_module_12.py
+++ practice_module_12.py
@@ -1,56 +1,42 @@
-import csv
-
-contacts = [
-    {
-        "name": "Allen Raymond",
-        "email": "nulla.ante@vestibul.co.uk",
-        "phone": "(992) 914-3792",
-        "favorite": False,
-    },
-    {
-        "name": "Pavlo Dubovyi",
-        "email": "pavlodubovyi@yahoo.com",
-        "phone": "1234567890",
-        "favorite": True,
-    },
-    {
-        "name": "John Doe",
-        "email": "john.doe@nowhere.to.go",
-        "phone": None,
-        "favorite": False,
-    },
-]
-
-filename = "my_contacts.csv"
+# import pickle
 
 
-def write_contacts_to_file(filename, contacts):
-    field_names = []
-    for el in contacts:
-        for key in el.keys():
-            field_names.append(key)
-        break
-
-    with open(filename, "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=field_names)
-        writer.writeheader()
-        writer.writerows(contacts)
+# class Human:
+#     def __init__(self, name):
+#         self.name = name
 
 
-def read_contacts_from_file(filename):
-    contact_list = []
-    with open(filename, "r", newline="") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            contact = {
-                "name": row["name"],
-                "email": row["email"],
-                "phone": row["phone"],
-                "favorite": row["favorite"] == "True",
-            }
-            contact_list.append(contact)
-    return contact_list
+# bob = Human("Bob")
+# encoded_bob = pickle.dumps(bob)
+
+# decoded_bob = pickle.loads(encoded_bob)
+
+# bob.name == decoded_bob.name  # True
+import pickle
 
 
-write_contacts_to_file(filename, contacts)
-print(read_contacts_from_file(filename))
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+        
+  
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        elif contacts:
+            
+        
+
+    def save_to_file(self):
+        
+            
+
+    def read_from_file(self):
+        
+            
+        
