e5cf7b64d6a66ba5a3be9693ed336097918f36fe
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-12-07 14:58:32 +0100

    half homework

--- Server_app/app.py
+++ Server_app/app.py
@@ -1,3 +1,4 @@
+import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import pathlib
 import urllib.parse  # для маршрутизації
@@ -7,29 +8,41 @@ BASE_DIR = pathlib.Path()
 
 
 class MyHTTPHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+
+    def do_POST(self):  # what client sends (server gets)
+
+        # body - це те, що нам введуть в полях Імʼя і Повідомлення
+        body = self.rfile.read(int(self.headers['Content-Length']))
+        new_body = urllib.parse.unquote_plus(body.decode())
+        payload = {key: value for key, value in [el.split('=') for el in new_body.split('&')]}
+        with open(BASE_DIR.joinpath('storage/data.json'), 'a+', encoding='utf-8') as filo:
+            json.dump(payload, filo, indent=2, ensure_ascii=False)  # ensure_ascii=False - щоб json розумів кирилицю
+        print(payload)
+
+        self.send_response(302)  # redirect to some page
+        self.send_header('location', '/')
+        self.end_headers()
+
+    def do_GET(self):  # what client gets (server sends)
         route = urllib.parse.urlparse(self.path)
         match route.path:
             case "/":
                 self.send_html('index.html')
             case "/contact":
-                self.send_html('contact.html')
-            case "/blog":
-                self.send_html('blog.html')
+                self.send_html('message.html')
             case _:
                 my_file = BASE_DIR / route.path[1:]
                 # print(my_file, f"FILE EXISTS: {my_file.exists()}")
                 if my_file.exists():
                     self.send_static(my_file)
                 else:
-                    self.send_html("404.html", 404)
-                self.send_html('404.html', 404)
+                    self.send_html("error.html", 404)
 
     def send_html(self, filename, status_code=200):
         self.send_response(status_code)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        with open(filename, 'rb') as file:  # відправляємо на всі запити тільки 'index.html'
+        with open(filename, 'rb') as file:
             self.wfile.write(file.read())
 
     def send_static(self, filename):
@@ -49,7 +62,7 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
 
 
 def run(server=HTTPServer, handler=MyHTTPHandler):
-    address = ('', 5001)  # '' = localhost
+    address = ('', 3000)  # '' = localhost
     http_server = server(address, handler)  # http server created
     try:  # http server launched
         print("Starting http server")
