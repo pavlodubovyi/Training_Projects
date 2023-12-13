0965eb8d551af0a7dc81b3bca178dba273de5a45
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-12-13 12:38:13 +0100

    more

---
+++ async_forever.py
@@ -0,0 +1,34 @@
+import asyncio
+from faker import Faker
+from time import sleep, time
+
+fake = Faker("uk-UA")
+
+
+async def async_user_from_db(uuid: int, future: asyncio.Future):
+    await asyncio.sleep(0.5)
+    future.set_result({"id": uuid, "username": fake.user_name(), "email": fake.email()})
+
+
+def make_request(uuid: int) -> asyncio.Future:
+    future = asyncio.Future()
+    asyncio.create_task(async_user_from_db(uuid, future))
+    return future
+
+
+async def main():
+    users = []
+    for i in range(1, 6):
+        users.append(make_request(i))
+    print([user.done() for user in users])
+    result = await asyncio.gather(*users)
+    print([user.done() for user in users])
+    return result
+
+
+if __name__ == "__main__":
+    print("Starting ASYNC...")
+    start = time()
+    our_users = asyncio.run(main())
+    print(our_users)
+    print(f"Result: {time() - start}")
